60d6d590b1713fbe60651a94513153669e309e78
anLA7856 <350493391@qq.com>
2017-10-14 23:29:01 +0800

    HashMap

--- src/main/java/com/blog/anla/sourceCode/TestCollection.java
+++ src/main/java/com/blog/anla/sourceCode/TestCollection.java
@@ -1,14 +1,13 @@
 package com.blog.anla.sourceCode;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Vector;
+import java.util.HashMap;
+import java.util.Map;
 
 
 
 public class TestCollection {
 	public static void main(String[] args) {
-		Vector<String> vector = new Vector<String>();
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>(5,0.5f);
+		System.out.println();
 	}
 }
