ba7c5d6c50f77f27585e71f29979e7c8f54386f8
anLA7856 <350493391@qq.com>
2017-10-15 21:37:59 +0800

    HashMap

--- src/main/java/com/blog/anla/sourceCode/TestCollection.java
+++ src/main/java/com/blog/anla/sourceCode/TestCollection.java
@@ -1,13 +1,62 @@
 package com.blog.anla.sourceCode;
 
 import java.util.HashMap;
-import java.util.Map;
-
-
 
 public class TestCollection {
+
 	public static void main(String[] args) {
-		Map<Integer, Integer> map = new HashMap<Integer, Integer>(5,0.5f);
-		System.out.println();
+		HashMap<Key, Key> map = new HashMap<Key, Key>();
+		for (int i = 0; i < 15; i++) {
+			map.put(new Key(i), new Key(i));
+			System.out.println(i);
+		}
+
+		java.util.TreeMap<Key, Key> tree = new java.util.TreeMap<Key, Key>();
+
+		for (int i = 0; i < 15; i++) {
+			tree.put(new Key(5), new Key(i));
+		}
+
+		System.out.println(map.containsValue(new String("3")));
 	}
 }
+
+class Key implements Comparable<Key> {
+	int key;
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Key other = (Key) obj;
+		if (key == other.key)
+			return true;
+		else
+			return true;
+	}
+
+	public Key(int key) {
+		this.key = key;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + key;
+		return result;
+	}
+
+	public int compareTo(Key o) {
+		if (this.key > o.key) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+}
+
